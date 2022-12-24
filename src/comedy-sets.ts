c17f9f9a49742e89bbfaff94cb82b1f3df298d34
megajon <jonathan.seubert@megajon.com>
2022-12-24 08:43:40 -0800

    first commit

---
+++ src/comedy-sets.ts
@@ -0,0 +1,12 @@
+export const videoContent = [
+  {
+    id: 0,
+    title: "Mommy Hoarder, Wierd Uber Deliveries",
+    url: "https://megajon-web.s3.amazonaws.com/videos/Trails+End+Golf+Center+-+8-6-2022.m4v"
+  },
+  {
+    id: 1,
+    title: "Facial Recognition Software, Best Panhandler (Bradley Cooper), Tiger Woods",
+    url: "https://megajon-web.s3.amazonaws.com/videos/Eastside+Bar+And+Grill+-+8-23-2022.m4v"
+  }
+]
