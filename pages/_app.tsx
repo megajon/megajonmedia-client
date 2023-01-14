6b2a91abfd5d430b35ac9d36c70188706d70371d
megajon <jonathan.seubert@megajon.com>
2023-01-14 12:04:02 -0800

    adds GA to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-19M4CNZQXJ" strategy="afterInteractive"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+  
+          gtag('config', 'G-19M4CNZQXJ');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
