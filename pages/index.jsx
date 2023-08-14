e599d745dcf8e65349377c893d2a1b906b89f0b0
megajon <jonathan.seubert@megajon.com>
2023-08-14 16:59:29 -0700

    adds no zoom meta to head 1

--- pages/index.jsx
+++ pages/index.jsx
@@ -58,6 +58,7 @@ export default function Home() {
   return (
     <>
       <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <title>Megajon Media</title>
       </Head>
       <main>
