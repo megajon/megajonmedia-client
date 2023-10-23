b8b990cd764f3dcf90c6dd586ab56d3422d8a108
megajon <jonathan.seubert@megajon.com>
2023-10-23 14:50:08 -0700

    switches order of comedy sets

--- components/MainContent.jsx
+++ components/MainContent.jsx
@@ -9,16 +9,16 @@ const MainContent = () => {
           <h3>Big Willy Bulger</h3>
         </div>
         <div className="comedy-set">
-          <video className="video"  poster="https://megajon-web.s3.amazonaws.com/images/trails_end_img.png" controls>
-            <source src="https://megajon-web.s3.amazonaws.com/videos/Trails+End+Golf+Center+-+8-6-2022.m4v" type="video/mp4" />
-          </video>
-          <h3>Facial Recognition, Black Bradley Cooper, Tiger Woods</h3>
-        </div>
-        <div className="comedy-set">
           <video className="video" poster="https://megajon-web.s3.amazonaws.com/images/east_side_1_img.png" controls>
             <source src="https://megajon-web.s3.amazonaws.com/videos/Eastside+Bar+And+Grill+-+8-23-2022.m4v" type="video/mp4" />
           </video>
           <h3>Mommy Hoarder, Weird Uber Deliveries</h3>
+        </div>
+        <div className="comedy-set">
+          <video className="video"  poster="https://megajon-web.s3.amazonaws.com/images/trails_end_img.png" controls>
+            <source src="https://megajon-web.s3.amazonaws.com/videos/Trails+End+Golf+Center+-+8-6-2022.m4v" type="video/mp4" />
+          </video>
+          <h3>Facial Recognition, Black Bradley Cooper, Tiger Woods</h3>
         </div>
       </div>
     </>
