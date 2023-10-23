b6dfe5f0f41bda5a5478d6c072565395d524f426
megajon <jonathan.seubert@megajon.com>
2023-10-23 14:42:21 -0700

    adds Big Willy Bulger video

--- components/MainContent.jsx
+++ components/MainContent.jsx
@@ -2,6 +2,12 @@ const MainContent = () => {
   return (
     <>
       <div id="main-content">
+      <div className="comedy-set">
+          <video className="video"  height="327px" poster="big_willy_bulger_poster.png" controls>
+            <source src="https://megajon-web.s3.amazonaws.com/videos/big_willy_bulger_instagram.mp4" type="video/mp4" />
+          </video>
+          <h3>Big Willy Bulger</h3>
+        </div>
         <div className="comedy-set">
           <video className="video"  poster="https://megajon-web.s3.amazonaws.com/images/trails_end_img.png" controls>
             <source src="https://megajon-web.s3.amazonaws.com/videos/Trails+End+Golf+Center+-+8-6-2022.m4v" type="video/mp4" />
